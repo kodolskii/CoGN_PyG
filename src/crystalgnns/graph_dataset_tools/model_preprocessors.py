63e0dab308d3c5c35d526b0a207d4b21ab57c10a
Vedant Kodolkar <93366911+kodolskii@users.noreply.github.com>
2024-01-18 12:50:43 +0530

    Update model_preprocessors.py

--- src/crystalgnns/graph_dataset_tools/model_preprocessors.py
+++ src/crystalgnns/graph_dataset_tools/model_preprocessors.py
@@ -14,10 +14,10 @@ from typing import Tuple, List
 # )
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
-from content/CoGN_PyG/src/crystalgnns.graph_dataset_tools.graph_tuple import GraphTuple
+from CoGN_PyG/src/crystalgnns.graph_dataset_tools.graph_tuple import GraphTuple
 # from kgcnn.graph.adj import get_angle_indices
 from enum import Enum
-from content/CoGN_PyG/src/crystalgnns.graph_dataset_tools.line_graph import get_line_graph
+from CoGN_PyG/src/crystalgnns.graph_dataset_tools.line_graph import get_line_graph
 
 
 class ModelPreprocessor:
